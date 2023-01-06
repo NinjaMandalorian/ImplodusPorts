45b9aa3327faa0cd48974172c359c1b2794c261c
Isaac <ninjamandalov@gmail.com>
2023-01-06 12:05:57 +0000

    Created main .java

---
+++ src/me/NinjaMandalorian/ImplodusPorts/ImpodusPorts.java
@@ -0,0 +1,15 @@
+package me.NinjaMandalorian.ImplodusPorts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ImpodusPorts extends JavaPlugin {
+    
+    public void onEnable() {
+        
+    }
+    
+    public void onDisable() {
+        
+    }
+    
+}
