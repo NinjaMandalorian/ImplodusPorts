615110d38ec82462804c3c9fa70b0afec62b97f3
Isaac <ninjamandalov@gmail.com>
2023-01-07 21:21:11 +0000

    Created construction functions

--- src/me/NinjaMandalorian/ImplodusPorts/ui/BaseButton.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/BaseButton.java
@@ -1,10 +1,18 @@
 package me.NinjaMandalorian.ImplodusPorts.ui;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 import me.NinjaMandalorian.ImplodusPorts.ui.tasks.BaseTask;
+import me.NinjaMandalorian.ImplodusPorts.ui.tasks.MessageTask;
+import net.md_5.bungee.api.ChatColor;
 
 /**
  * Represents a button in the Inventory Menu
@@ -17,20 +25,7 @@ public class BaseButton {
     private BaseTask task;
     private HashMap<String,String> metadata;
     
-    public BaseButton(ItemStack itemStack, BaseTask task, HashMap<String, String> metadata) {
-        this.itemStack = itemStack;
-        this.task = task;
-        this.metadata = metadata;
-    }
-
-    
-    public BaseButton(ItemStack itemStack, BaseTask task) {
-        this(itemStack, task, new HashMap<String,String>());
-    }
-    
-    public ItemStack getItemStack() { 
-        return this.itemStack;
-    }
+    private BaseButton() {};
     
     public BaseTask getTask() {
         return this.task;
@@ -42,6 +37,67 @@ public class BaseButton {
     
     public void setMetadata(HashMap<String, String> map) {
         this.metadata = map;
+    }
+    
+    public ItemStack getItemStack() { 
+        return this.itemStack;
+    }
+    
+    // Constructors for building
+    
+    public static BaseButton create() {
+        BaseButton button = new BaseButton();
+        button.itemStack = new ItemStack(Material.GLASS);
+        button = button.name(ChatColor.GRAY + "Empty");
+        button.task = new MessageTask("Task-Unassigned");
+        return button;
+    }
+    
+    public static BaseButton create(Material material) {
+        BaseButton button = create();
+        button.itemStack = new ItemStack(material);
+        return button;
+    }
+    
+    public BaseButton glow() {
+        this.itemStack.addUnsafeEnchantment(Enchantment.LUCK, 1);
+        ItemMeta meta = this.itemStack.getItemMeta();
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        this.itemStack.setItemMeta(meta);
+        return this;
+    }
+    
+    public BaseButton itemStack(ItemStack itemStack) {
+        this.itemStack = itemStack;
+        return this;
+    }
+    
+    public BaseButton task(BaseTask task) {
+        this.task = task;
+        return this;
+    }
+    
+    public BaseButton quantity(int num) {
+        this.itemStack.setAmount(num);
+        return this;
+    }
+    
+    public BaseButton name(String name) {
+        ItemMeta meta = this.itemStack.getItemMeta();
+        meta.setDisplayName(ChatColor.RESET + name);
+        this.itemStack.setItemMeta(meta);
+        return this;
+    }
+    
+    public BaseButton lore(List<String> lore) {
+        ItemMeta meta = this.itemStack.getItemMeta();
+        meta.setLore(lore);
+        this.itemStack.setItemMeta(meta);
+        return this;
+    }
+    
+    public BaseButton lore(String lore) {
+        return lore(Arrays.asList(lore.split("\n")));
     }
     
 }
