ac54067d7b8702d6dd86ab33dc92f23ff88ffe03
Isaac <ninjamandalov@gmail.com>
2023-01-07 12:46:52 +0000

    Creating Base Task interface
    This interface is used for slots within the menu, allowing for a centralized running of several different types of function through one mainframe.

---
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/tasks/BaseTask.java
@@ -0,0 +1,11 @@
+package me.NinjaMandalorian.ImplodusPorts.ui.tasks;
+
+/**
+ * Tasks are assigned to slots in the BaseMenu.
+ * @author NinjaMandalorian
+ *
+ */
+public interface BaseTask {
+    
+    
+}
