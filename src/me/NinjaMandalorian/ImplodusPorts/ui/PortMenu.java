7bcbd57ad57cc2cb3e7bc9496b53edc70888d405
Isaac <ninjamandalov@gmail.com>
2023-01-07 21:21:37 +0000

    Implemented new construction functions

--- src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
@@ -1,11 +1,7 @@
 package me.NinjaMandalorian.ImplodusPorts.ui;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
-
 import me.NinjaMandalorian.ImplodusPorts.object.Port;
 import me.NinjaMandalorian.ImplodusPorts.ui.tasks.MessageTask;
 
@@ -14,9 +10,7 @@ public class PortMenu {
     public static BaseMenu createPortMenu(Player player, Port port) {
         
         BaseMenu builderMenu = BaseMenu.createBuilder()
-                .setButton(0, new BaseButton(
-                        new ItemStack(Material.BIRCH_BOAT), new MessageTask("Birch boat!"))
-                        )
+                .setButton(0, BaseButton.create(Material.ACACIA_BOAT).glow().task(new MessageTask("Boing")))
                 .title("Bingle bongle")
                 .rows(5)
                 .build();
