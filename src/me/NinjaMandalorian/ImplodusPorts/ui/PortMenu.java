7ebb235122a7a1adc531556748339c37388003fa
Isaac <ninjamandalov@gmail.com>
2023-01-07 14:37:14 +0000

    Changed to builder structure

--- src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
@@ -7,16 +7,19 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
 import me.NinjaMandalorian.ImplodusPorts.object.Port;
+import me.NinjaMandalorian.ImplodusPorts.ui.tasks.MessageTask;
 
 public class PortMenu {
 
     public static BaseMenu createPortMenu(Player player, Port port) {
         
+        BaseMenu builderMenu = BaseMenu.createBuilder()
+                .setButton(0, new BaseButton(
+                        new ItemStack(Material.BIRCH_BOAT), new MessageTask("Birch boat!"))
+                        )
+                .build();
         
-        Inventory inventory = Bukkit.createInventory(null, 54);
-        inventory.setItem(4, new ItemStack(Material.ACACIA_BOAT));
-        
-        return new BaseMenu(inventory, "test-title");
+        return builderMenu;
             
     }
     
