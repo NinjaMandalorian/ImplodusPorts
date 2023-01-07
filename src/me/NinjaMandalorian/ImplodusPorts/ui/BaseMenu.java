063dad4a3eedda52863447d06c456fb0e29c66f8
Isaac <ninjamandalov@gmail.com>
2023-01-07 02:22:29 +0000

    open() menu

--- src/me/NinjaMandalorian/ImplodusPorts/ui/BaseMenu.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/BaseMenu.java
@@ -1,6 +1,7 @@
 package me.NinjaMandalorian.ImplodusPorts.ui;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
 
@@ -10,8 +11,12 @@ public class BaseMenu implements InventoryHolder {
     
     public BaseMenu(Inventory inventory, String menuTitle) {
         // Re-creates inventory with BaseMenu as holder
-        this.inventory = Bukkit.createInventory(this, inventory.getSize());
+        this.inventory = Bukkit.createInventory(this, inventory.getSize(), menuTitle);
         this.inventory.setContents(inventory.getContents());
+    }
+    
+    public void open(Player player) {
+        player.openInventory(inventory);
     }
     
     @Override
