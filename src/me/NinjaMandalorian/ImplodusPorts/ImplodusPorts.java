3cb043c479f8c46a0657d58eec77c42ba66c7243
Isaac <ninjamandalov@gmail.com>
2023-01-07 02:25:44 +0000

    Added init() for data

--- src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
@@ -1,6 +1,14 @@
 package me.NinjaMandalorian.ImplodusPorts;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.NinjaMandalorian.ImplodusPorts.command.ImplodusPortsCommands;
+import me.NinjaMandalorian.ImplodusPorts.data.DataManager;
+import me.NinjaMandalorian.ImplodusPorts.object.IconMenu;
+import me.NinjaMandalorian.ImplodusPorts.settings.Settings;
 
 public class ImplodusPorts extends JavaPlugin {
     
@@ -8,10 +16,16 @@ public class ImplodusPorts extends JavaPlugin {
     
     public void onEnable() {
         instance = this;
+        
+        // Initialise parts of the plugin
+        DataManager.init();
+        Settings.init();
+        
+        new ImplodusPortsCommands();
     }
     
     public void onDisable() {
-        
+        Bukkit.getLogger().info("Disabling ImplodusPorts");
     }
     
     public static ImplodusPorts getInstance() {
