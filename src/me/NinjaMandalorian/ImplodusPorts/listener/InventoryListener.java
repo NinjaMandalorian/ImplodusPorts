76c31a09823a8e7c7284b16f5f2d6531f2e04c0c
Isaac <ninjamandalov@gmail.com>
2023-01-07 13:33:56 +0000

    Added logic to InventoryClickEvent
    - If inventory held by a BaseMenu, immediately cancels
    - Gets slot number and sends for button with corresponding button.

--- src/me/NinjaMandalorian/ImplodusPorts/listener/InventoryListener.java
+++ src/me/NinjaMandalorian/ImplodusPorts/listener/InventoryListener.java
@@ -5,12 +5,24 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+import me.NinjaMandalorian.ImplodusPorts.ui.BaseMenu;
+
 public class InventoryListener implements Listener {
     
+    /**
+     * Handles inventories being clicked --> If inventory held by BaseMenu, corresponding task is run.
+     * @param e - Event
+     */
     @EventHandler
     public void onInventoryClick(InventoryClickEvent e) {
-        if (e.getInventory().getHolder() == null) return;
-        Bukkit.getLogger().info( e.getInventory().getHolder().getClass().toString() );
-        e.getInventory();
+        if (e.getInventory().getHolder() instanceof BaseMenu menu) {
+            e.setCancelled(true); // Immediately cancels any movement of items.
+            
+            // If outside of custom inventory, returns.
+            int slotNum = e.getRawSlot();
+            if (slotNum + 1 > e.getInventory().getSize()) return;
+            
+            menu.getButton(slotNum);
+        } else return;
     }
 }
