170d9940478b7c5ff1f6212f3d2cc0f3b3bf08b9
Isaac <ninjamandalov@gmail.com>
2023-01-07 12:50:54 +0000

    BaseButton creation
    - Used in place of an ItemStack to also contain a task & other operator data
    - Stored in a menu instance with a BaseButton[]
    - Contains ItemStack as well.

---
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/BaseButton.java
@@ -0,0 +1,49 @@
+package me.NinjaMandalorian.ImplodusPorts.ui;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+import org.bukkit.inventory.ItemStack;
+
+import me.NinjaMandalorian.ImplodusPorts.ui.tasks.BaseTask;
+
+/**
+ * Represents a button in the Inventory Menu
+ * Contains the ItemStack, BaseTask & Any other metadata.
+ * @author NinjaMandalorian
+ */
+public class BaseButton {
+    
+    private ItemStack itemStack;
+    private BaseTask task;
+    private HashMap<String,String> metadata;
+    
+    public BaseButton(ItemStack itemStack, BaseTask task, HashMap<String, String> metadata) {
+        this.itemStack = itemStack;
+        this.task = task;
+        this.metadata = metadata;
+    }
+
+    
+    public BaseButton(ItemStack itemStack, BaseTask task) {
+        this(itemStack, task, new HashMap<String,String>());
+    }
+    
+    public ItemStack getItemStack() { 
+        return this.itemStack;
+    }
+    
+    public BaseTask getTask() {
+        return this.task;
+    }
+    
+    public HashMap<String, String> getMetadata() {
+        return this.metadata;
+    }
+    
+    public void setMetadata(HashMap<String, String> map) {
+        this.metadata = map;
+    }
+    
+}
