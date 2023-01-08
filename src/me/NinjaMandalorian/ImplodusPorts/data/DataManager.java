cc5392e1560725f6fb8137462a6852fda2105630
Isaac <ninjamandalov@gmail.com>
2023-01-08 16:03:50 +0000

    createFolders()

--- src/me/NinjaMandalorian/ImplodusPorts/data/DataManager.java
+++ src/me/NinjaMandalorian/ImplodusPorts/data/DataManager.java
@@ -1,15 +1,36 @@
 package me.NinjaMandalorian.ImplodusPorts.data;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
 import me.NinjaMandalorian.ImplodusPorts.ImplodusPorts;
 
 public class DataManager {
     
     private static ImplodusPorts plugin;
     
+    private static String dataFolder = "plugins" + File.separator + "ImplodusNuclei" + File.separator + "data";
+    
     public static void init(){
         plugin = ImplodusPorts.getInstance();
         
+        createFolders();
+        
         plugin.getConfig();
+    }
+
+    private static void createFolders() {
+        List<String> paths = Arrays.asList(
+                "plugins" + File.separator + "ImplodusPorts" + File.separator + "data"
+                );
+        
+        for (String path : paths) {
+            File file = new File(path);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+        }
     }
     
 }
