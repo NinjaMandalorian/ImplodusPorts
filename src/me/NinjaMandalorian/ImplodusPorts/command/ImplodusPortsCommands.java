c1668f429d449675ee079bf7fc777836c4a5399d
Isaac <ninjamandalov@gmail.com>
2023-01-10 10:38:06 +0000

    Updated for testing

--- src/me/NinjaMandalorian/ImplodusPorts/command/ImplodusPortsCommands.java
+++ src/me/NinjaMandalorian/ImplodusPorts/command/ImplodusPortsCommands.java
@@ -2,6 +2,8 @@ package me.NinjaMandalorian.ImplodusPorts.command;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -9,6 +11,8 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import me.NinjaMandalorian.ImplodusPorts.ImplodusPorts;
+import me.NinjaMandalorian.ImplodusPorts.helper.StringHelper;
+import me.NinjaMandalorian.ImplodusPorts.object.Port;
 import me.NinjaMandalorian.ImplodusPorts.ui.PortMenu;
 
 public class ImplodusPortsCommands implements CommandExecutor, TabCompleter {
@@ -27,9 +31,7 @@ public class ImplodusPortsCommands implements CommandExecutor, TabCompleter {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("HAHA");
         if (sender instanceof Player player) {
-            PortMenu.createPortMenu(player, null).open(player);
             if (args.length == 0) {
                 return false;
             }
@@ -40,7 +42,7 @@ public class ImplodusPortsCommands implements CommandExecutor, TabCompleter {
                 return true;
             case "debug":
                 args = StringHelper.remFirst(args);
-                Port fakePort = new Port("bingus_town", new Location(Bukkit.getWorld("World"), 0.0, 0.0, 0.0), args.length > 0 ? Integer.parseInt(args[0]) : 1, "Bingus Town");
+                Port fakePort = new Port("bingus_town", new Location(Bukkit.getWorld("World"), 0.0, 0.0, 0.0), null, 4, "Bingus Town");
                 PortMenu.createPortMenu(player, fakePort).open(player);
                 return true;
             default:
