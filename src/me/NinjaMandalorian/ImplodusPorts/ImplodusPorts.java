75b83ae1b85ddff95f39e63c3f6d077075d11e28
Isaac <ninjamandalov@gmail.com>
2023-01-10 09:53:12 +0000

    Added economy

--- src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
@@ -2,12 +2,14 @@ package me.NinjaMandalorian.ImplodusPorts;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.NinjaMandalorian.ImplodusPorts.command.ImplodusPortsCommands;
 import me.NinjaMandalorian.ImplodusPorts.data.DataManager;
 import me.NinjaMandalorian.ImplodusPorts.listener.InventoryListener;
 import me.NinjaMandalorian.ImplodusPorts.settings.Settings;
+import net.milkbowl.vault.economy.Economy;
 
 /**
  * Main class for ImplodusPorts plugin
@@ -17,6 +19,7 @@ import me.NinjaMandalorian.ImplodusPorts.settings.Settings;
 public class ImplodusPorts extends JavaPlugin {
     
     private static ImplodusPorts instance;
+    public static Economy econ;
     
     public void onEnable() {
         instance = this;
@@ -24,6 +27,7 @@ public class ImplodusPorts extends JavaPlugin {
         // Initialise parts of the plugin
         DataManager.init();
         Settings.init();
+        setupEconomy();
         
         new ImplodusPortsCommands();
         
@@ -33,6 +37,18 @@ public class ImplodusPorts extends JavaPlugin {
     
     public void onDisable() {
         Bukkit.getLogger().info("Disabling ImplodusPorts");
+    }
+    
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
     }
     
     public static ImplodusPorts getInstance() {
