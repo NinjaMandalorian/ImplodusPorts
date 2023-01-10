867062f4a9347680f6f0a9aebdaad490911e6f0b
Isaac <ninjamandalov@gmail.com>
2023-01-10 00:16:21 +0000

    Finished basis for port menu

--- src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ui/PortMenu.java
@@ -9,6 +9,32 @@ public class PortMenu {
 
     public static BaseMenu createPortMenu(Player player, Port port) {
         
+        Builder builder = BaseMenu.createBuilder()
+                .setButton(4, portToButton(port, port))
+                .setButton(49, BaseButton.create(Material.OAK_CHEST_BOAT).glow()
+                        .name("&o&aSee all ports")
+                        .task(new InventoryTask(createPortGlobalMenu(player, port))))
+                .title("&9Port - " + port.getDisplayName())
+                .openMsg("&aOpening port &9" + port.getDisplayName() + "&a...")
+                .fillOutline()
+                ;
+        
+        
+        int portCount = 0;
+        for (Port availablePort : port.getNearby()) {
+            List<Integer> slotList = Arrays.asList(
+            10,11,12,13,14,15,16,
+            19,20,21,22,23,24,25,
+            28,29,30,31,32,33,34,
+            37,38,39,40,41,42,43);
+            
+            int slot = slotList.get(portCount);
+            
+            builder = builder.setButton(slot, portToButton(port, availablePort));
+            portCount++;
+        }
+        
+        return builder.build();
         BaseMenu builderMenu = BaseMenu.createBuilder()
                 .setButton(0, BaseButton.create(Material.ACACIA_BOAT).glow().task(new MessageTask("Boing")))
                 .title("Bingle bongle")
