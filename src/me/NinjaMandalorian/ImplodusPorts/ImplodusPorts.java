bdb6aed0d37ef040fea6eec565d3018a5b9c6f79
Isaac <ninjamandalov@gmail.com>
2023-01-11 16:53:03 +0000

    Implement PlayerListener() & Port.initPorts()

--- src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
+++ src/me/NinjaMandalorian/ImplodusPorts/ImplodusPorts.java
@@ -1,13 +1,17 @@
 package me.NinjaMandalorian.ImplodusPorts;
 
 import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginLogger;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import me.NinjaMandalorian.ImplodusPorts.command.ImplodusPortsCommands;
 import me.NinjaMandalorian.ImplodusPorts.data.DataManager;
+import me.NinjaMandalorian.ImplodusPorts.data.PortDataManager;
 import me.NinjaMandalorian.ImplodusPorts.listener.InventoryListener;
+import me.NinjaMandalorian.ImplodusPorts.listener.PlayerListener;
+import me.NinjaMandalorian.ImplodusPorts.object.Port;
 import me.NinjaMandalorian.ImplodusPorts.settings.Settings;
 import net.milkbowl.vault.economy.Economy;
 
@@ -27,12 +31,14 @@ public class ImplodusPorts extends JavaPlugin {
         // Initialise parts of the plugin
         DataManager.init();
         Settings.init();
+        Port.initPorts();
         setupEconomy();
         
         new ImplodusPortsCommands();
         
         PluginManager PluginManager = getServer().getPluginManager();
         PluginManager.registerEvents(new InventoryListener(), instance);
+        PluginManager.registerEvents(new PlayerListener(), instance);
     }
     
     public void onDisable() {
