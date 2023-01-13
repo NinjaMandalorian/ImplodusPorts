8d1ff944e1c876d9840993cfc8dc615ccf967990
Isaac <ninjamandalov@gmail.com>
2023-01-13 23:14:23 +0000

    portFromSign()

--- src/me/NinjaMandalorian/ImplodusPorts/helper/PortHelper.java
+++ src/me/NinjaMandalorian/ImplodusPorts/helper/PortHelper.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import org.bukkit.block.Sign;
+import org.bukkit.entity.Player;
+
 import me.NinjaMandalorian.ImplodusPorts.object.Port;
 
 public class PortHelper {
@@ -43,6 +48,16 @@ public class PortHelper {
         returnList.addAll(sortedMap.values());
         
         return returnList;
+    }
+
+    public static Port portFromSign(Player player, Block block, String[] lines) {
+        String id = lines[1].toLowerCase().strip().replace(' ', '_');
+        String displayName = StringHelper.capitalize(lines[1].strip().replace('_', ' '));
+        Location signLocation = block.getLocation();
+        Location teleportLocation = player.getLocation();
+        
+        Port port = new Port(id, signLocation, teleportLocation, 1, displayName);
+        return port;
     }
     
 }
