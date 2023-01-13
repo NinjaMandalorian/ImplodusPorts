57df6fb2d13547731823ffd039fffcf0a862638b
Isaac <ninjamandalov@gmail.com>
2023-01-13 23:33:55 +0000

    log to not error

--- src/me/NinjaMandalorian/ImplodusPorts/Logger.java
+++ src/me/NinjaMandalorian/ImplodusPorts/Logger.java
@@ -4,6 +4,6 @@ import org.bukkit.Bukkit;
 
 public class Logger {
     public static void log(String string) {
-        Bukkit.getLogger().severe(string);
+        Bukkit.getLogger().info(string);
     }
 }
