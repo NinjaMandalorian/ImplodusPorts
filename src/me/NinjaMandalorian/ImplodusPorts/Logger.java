60d59eec4dd07d86436ef7c6d16342c90885f5e5
Isaac <ninjamandalov@gmail.com>
2023-01-14 00:35:44 +0000

    Update Logger.java

--- src/me/NinjaMandalorian/ImplodusPorts/Logger.java
+++ src/me/NinjaMandalorian/ImplodusPorts/Logger.java
@@ -5,5 +5,14 @@ import org.bukkit.Bukkit;
 public class Logger {
     public static void log(String string) {
         Bukkit.getLogger().info(string);
+        quietLog("[INFO] " + string);
+    }
+    
+    public static void warn(String string) {
+        quietLog("[WARN] " + string);
+    }
+    
+    public static void quietLog(String string) {
+        // Insert .txt logging system
     }
 }
