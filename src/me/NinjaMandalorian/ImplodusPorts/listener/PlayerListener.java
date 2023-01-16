827b70d401c512e05bf44c40b6f82bd8afe288cb
Isaac <ninjamandalov@gmail.com>
2023-01-16 04:23:30 +0000

    Update PlayerListener.java

--- src/me/NinjaMandalorian/ImplodusPorts/listener/PlayerListener.java
+++ src/me/NinjaMandalorian/ImplodusPorts/listener/PlayerListener.java
@@ -8,6 +8,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 import me.NinjaMandalorian.ImplodusPorts.object.Port;
 import me.NinjaMandalorian.ImplodusPorts.ui.PortMenu;
@@ -39,5 +40,10 @@ public class PlayerListener implements Listener {
                 return;
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerMove(PlayerMoveEvent e) {
+        Player player = e.getPlayer();
     }
 }
